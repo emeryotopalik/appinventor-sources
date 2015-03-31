32b128065174139003752dcd6caf71116adc041a
emeryotopalik <emerycgo@gmail.com>
2015-03-31 11:53:03 -0400

    "JSON display changes"

--- appinventor/buildserver/src/com/google/appinventor/buildserver/resources/runtime.scm
+++ appinventor/buildserver/src/com/google/appinventor/buildserver/resources/runtime.scm
@@ -1378,7 +1378,44 @@
 ;;; be explicity shown in error messages.
 ;;; This procedure is currently almost completely redundant with coerce-to-string
 ;;; but it give us flexibility to tailor display for other data types
-(define get-display-representation
+
+;; What we eventually want to get:
+;; (foo bar baz) -> ["foo", "bar", "baz"]
+
+(define get-original-display-representation
+ ;;  there seems to be a bug in Kawa that makes (/ -1 0) equal to (/ 1 0)
+ ;; which is why this uses 1.0 and -1.0
+  (let ((+inf (/ 1.0 0))
+        (-inf (/ -1.0 0)))
+    (lambda (arg)
+      (cond ((= arg +inf) "+infinity")
+            ((= arg -inf) "-infinity")
+            ((eq? arg *the-null-value*) *the-null-value-printed-rep*)
+            ((symbol? arg)
+             (symbol->string arg))
+           ((string? arg ) (string-append "\"" arg "\""))
+            ;; (if (string=? arg "")
+              ;   *the-empty-string-printed-rep*
+               ;;  (string-append "\"" (string-append arg "\"")))) ;EMERY
+            ((number? arg) (appinventor-number->string arg))
+            ((boolean? arg) (boolean->string arg))
+            ((yail-list? arg) (get-display-representation (yail-list->kawa-list arg))) 
+            ((list? arg)
+             (let ((pieces (map get-display-representation arg)))
+               (call-with-output-string (lambda (port) (display pieces port)))))
+            (else (call-with-output-string (lambda (port) (display arg port))))))))
+
+;;; This is very similar to coerce-to-string, but is intended for places where we
+;;; want to make the structure more clear.  For example, the empty string should
+;;; be explicity shown in error messages.
+;;; This procedure is currently almost completely redundant with coerce-to-string
+;;; but it give us flexibility to tailor display for other data types
+ (define get-display-representation 
+   (lambda (arg) (get-json-display-representation arg))
+    ;;(lambda (arg) (get-original-display-representation arg))
+ )
+
+(define get-json-display-representation
   ;; there seems to be a bug in Kawa that makes (/ -1 0) equal to (/ 1 0)
   ;; which is why this uses 1.0 and -1.0
   (let ((+inf (/ 1.0 0))
@@ -1389,17 +1426,25 @@
             ((eq? arg *the-null-value*) *the-null-value-printed-rep*)
             ((symbol? arg)
              (symbol->string arg))
-            ((string? arg )
-             (if (string=? arg "")
-                 *the-empty-string-printed-rep*
-                 (string-append "\"" (string-append arg "\"")))) ;EMERY
+            ((string? arg) (string-append "\"" arg "\""))
             ((number? arg) (appinventor-number->string arg))
             ((boolean? arg) (boolean->string arg))
-            ((yail-list? arg) (get-display-representation (yail-list->kawa-list arg)))
+            ((yail-list? arg) (get-json-display-representation (yail-list->kawa-list arg)))
             ((list? arg)
-             (let ((pieces (map get-display-representation arg)))
-               (call-with-output-string (lambda (port) (display pieces port)))))
+             (let ((pieces (map get-json-display-representation arg)))
+                (string-append "[" (join-strings pieces ", ") "]")))
             (else (call-with-output-string (lambda (port) (display arg port))))))))
+
+(define (join-strings strings separator)
+   (cond ((null? strings) "")
+         ((null? (cdr strings)) (car strings))
+         (else ;; have at least two strings
+           (apply string-append
+                  (cons (car strings)
+                        (let recur ((strs (cdr strings)))
+                          (if (null? strs)
+                              '()
+                              (cons separator (cons (car strs) (recur (cdr strs)))))))))))
 
 ;; Note: This is not general substring replacement. It just replaces one string with another
 ;; using the replacement table
