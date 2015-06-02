77342a5c211d93ae526a4cf917f45952b9275b1c
emeryotopalik <emerycgo@gmail.com>
2015-06-02 09:07:11 -0400

    adding watch function

--- appinventor/lib/blockly/src/core/generator.js
+++ appinventor/lib/blockly/src/core/generator.js
@@ -154,14 +154,24 @@ Blockly.Generator.prototype.blockToCode = function(block) {
   // The current prefered method of accessing the block is through the second
   // argument to func.call, which becomes the first parameter to the generator.
   var code = func.call(block, block);
-  if (goog.isArray(code)) {
+  if (goog.isArray(code)) {   // edited by Emery to include watch function, 6/2/15
     // Value blocks return tuples of code and operator order.
+    var generatedCode = code[0];
+    if (block.watch) {
+      generatedCode = "(watch " + block.id + " " + generatedCode + ")";
+    } else {
+      generatedCode =  "(augment " + block.id + " " + generatedCode + ")";
+    }
+    return [this.scrub_(block, generatedCode), code[1]];
     //[12.2.13] added augment tag to keep track of block id when yail is sent to device
-    var result = [this.scrub_(block, "(augment " + block.id + " " + code[0] + ")"), code[1]];
+   // var result = [this.scrub_(block, "(augment " + block.id + " " + code[0] + ")"), code[1]];
     //console.log("Output code: [" + result + "]");
-    return result;
+   // return result;
     //return ["(augment " + block.id + " " + (this.scrub_(block, code[0])) + ")", code[1]];
   } else if (goog.isString(code)) {
+    if (block.watch) {
+      code = "(watch " + block.id + " " +  code +")";
+    }
     if (this.STATEMENT_PREFIX) {
       code = this.STATEMENT_PREFIX.replace(/%1/g, '\'' + block.id + '\'') +
           code;
