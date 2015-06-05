8797b6b76d23233804b89df3fb38acb49c22b50b
emeryotopalik <emerycgo@gmail.com>
2015-06-05 12:27:57 -0400

    some watch debugging

--- appinventor/lib/blockly/src/core/block.js
+++ appinventor/lib/blockly/src/core/block.js
@@ -1998,6 +1998,11 @@ Blockly.Block.prototype.setCommentText = function(text) {
   }
 };
 
+//Emery
+Blockly.Block.prototype.setFocus = function(e) {
+   this.comment.textareaFocus_();
+}
+
 /**
  * Set this block's warning text.
  * @param {?string} text The text, or null to delete.
